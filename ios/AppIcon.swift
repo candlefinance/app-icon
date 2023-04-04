eb1b220e215c6bc626ee5b9dc35976c17b6e48c6
Gary Tokman <hello@garytokman.me>
2023-04-04 13:04:17 -0400

    feat: update icons on main thread

--- ios/AppIcon.swift
+++ ios/AppIcon.swift
@@ -1,28 +1,31 @@
 @objc(AppIcon)
 class AppIcon: NSObject {
-
-  @objc(getIconName:withRejecter:)
-  func getIconName(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
-    if UIApplication.shared.supportsAlternateIcons {
-      resolve(UIApplication.shared.alternateIconName ?? "default")
-    } else {
-      reject("supportsAlternateIcons:false", "Alternate icons are not supported on this device. Make sure you followed the README and configured your project correctly.", nil)
-    }
-  }
-
-  @objc(setIconName:withResolver:withRejecter:)
-  func setIconName(name: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-    if UIApplication.shared.supportsAlternateIcons {
-      UIApplication.shared.setAlternateIconName(name == "default" ? nil : name) { error in
-        if let error = error {
-          reject("setAlternateIconName:error", "Failed to set alternate icon name with error: \(error.localizedDescription)", error)
-        } else {
-          resolve(name)
+    
+    @objc(getIconName:withRejecter:)
+    func getIconName(resolve: @escaping RCTPromiseResolveBlock,  reject: @escaping RCTPromiseRejectBlock) -> Void {
+        DispatchQueue.main.async {
+            if UIApplication.shared.supportsAlternateIcons {
+                resolve(UIApplication.shared.alternateIconName ?? "default")
+            } else {
+                reject("supportsAlternateIcons:false", "Alternate icons are not supported on this device. Make sure you followed the README and configured your project correctly.", nil)
+            }
         }
-      }
-    } else {
-      reject("supportsAlternateIcons:false", "Alternate icons are not supported on this device. Make sure you followed the README and configured your project correctly.", nil)
     }
-  }
-
+    
+    @objc(setIconName:withResolver:withRejecter:)
+    func setIconName(name: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+        DispatchQueue.main.async {
+            if UIApplication.shared.supportsAlternateIcons {
+                UIApplication.shared.setAlternateIconName(name == "default" ? nil : name) { error in
+                    if let error = error {
+                        reject("setAlternateIconName:error", "Failed to set alternate icon name with error: \(error.localizedDescription)", error)
+                    } else {
+                        resolve(name)
+                    }
+                }
+            } else {
+                reject("supportsAlternateIcons:false", "Alternate icons are not supported on this device. Make sure you followed the README and configured your project correctly.", nil)
+            }
+        }
+    }
 }
